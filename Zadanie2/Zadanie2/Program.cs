74ef858d0a76ed9fd9dcf712533d0f0f838e9c61
S28548 <s28548@pjwstk.edu.pl>
2025-03-03 13:06:27 +0100

    Loop iterator change on main

--- Zadanie2/Zadanie2/Program.cs
+++ Zadanie2/Zadanie2/Program.cs
@@ -5,9 +5,9 @@ double CalculateAverage(int[] numbers)
 {
     int sum = 0;
 
-    for (int i = 0; i < numbers.Length; i++)
+    for (int k = 0; k < numbers.Length; k++)
     {
-        sum += numbers[i];
+        sum += numbers[k];
     }
 
     return sum / numbers.Length;
